f30a55e05f46fae5a5d4874f1350cfc6fe8442ce
ahmedhabeeb73 <alnaib888@gmail.com>
2020-12-01 16:22:00 +0100

    add 3 data tables for stores products orders

--- database/migrations/2020_12_01_150507_create_stores_table.php
+++ database/migrations/2020_12_01_150507_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('number');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('image')->nullable();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')
+                   ->references('id')
+                   ->on('users')
+                   ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -32,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 }
--- database/migrations/2020_12_01_150939_create_products_table.php
+++ database/migrations/2020_12_01_150939_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('number');
-            $table->rememberToken();
+            $table->decimal('price',8,3);
+            $table->string('images')->nullable();
+
+            $table->unsignedBigInteger('store_id');
+
+            $table->foreign('store_id')
+                   ->references('id')
+                   ->on('stores')
+                   ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -32,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
